aea979229e2ccf6678aae4ca9e434808daa62a60
Sameera Challapalli <84790812+sameera18@users.noreply.github.com>
2023-06-27 13:43:51 +0530

    Add files via upload

---
+++ SearchElement.py
@@ -0,0 +1,26 @@
+class Solution:
+    #Complete the below function
+    def search(self,arr, N, X):
+        #Your code here
+        if  X in arr:
+            return arr.index(X)
+        else:
+            return -1
+ # Driver Code Starts
+import math   
+def main():
+        T=int(input())
+        while(T>0):
+            
+            N=int(input())
+            
+            A=[int(x) for x in input().strip().split()]
+            
+            x=int(input())
+            ob=Solution()
+            print(ob.search(A,N,x))
+            
+            T-=1
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
